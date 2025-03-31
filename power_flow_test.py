436d27de6779a70fb8f4401ee1f74ebb630b81cc
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2025-03-31 15:29:52 +1100

    Testing new GPG key :)

--- power_flow_test.py
+++ power_flow_test.py
@@ -7,7 +7,7 @@ pp.create_line(net, from_bus=slack_bus, to_bus=b2, length_km=2.5, std_type="NAYY
 pp.create_line(net, from_bus=slack_bus, to_bus=b3, length_km=2.5, std_type="NAYY 4x50 SE")      
 pp.create_ext_grid(net, bus=slack_bus)
 pp.create_load(net, bus=b2, p_mw=1.5, q_mvar=0.5)
-pp.create_load(net, bus=b3, p_mw=1.)
+pp.create_load(net, bus=b3, p_mw=1.0)
 pp.runpp(net)
 print(net.res_bus.vm_pu)
 print(net.res_bus)
