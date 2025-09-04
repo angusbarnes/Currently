cb5f51e7a8e06c99dbd4c4d85d7835d0a57452ee
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2025-09-04 21:48:31 +1000

    Network creation functions

---
+++ server/network.py
@@ -0,0 +1,202 @@
+from dataclasses import dataclass
+from pathlib import Path
+import csv
+import logging
+import pandapower as pp 
+import pandas as pd
+
+from typing import List, Dict
+
+def string_to_bool(s):
+    s = s.lower()
+    if s in ("true", "1", "t"):
+        return True
+    elif s in ("false", "0", "f", ""):
+        return False
+    else:
+        raise ValueError(f"Invalid boolean string: '{s}'")
+
+@dataclass
+class LineType:
+    name: str
+    c_nf_per_km: float
+    r_ohm_per_km: float
+    x_ohm_per_km: float
+    max_i_ka: float
+    q_mm2: float # Conductor cross sectional area
+    alpha: float # Temperature coeff for cable
+    type: str = "cs"
+
+def load_cable_types(cable_file: Path):
+    custom_line_types = []
+    with open(cable_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            custom_line_types.append(row)
+    
+    known_cables = []
+
+    for cable in custom_line_types:
+        try:
+            known_cables.append(LineType(
+                name=cable["Cable Name"],
+                c_nf_per_km= float(cable["C (nF/km)"]),
+                r_ohm_per_km= float(cable["R (Ohm/km)"]),
+                x_ohm_per_km= float(cable["X (j Ohm/km)"]),
+                max_i_ka= float(cable["Max I (kA)"]),
+                q_mm2= float(cable["Q (mm2)"]),
+                alpha= float(cable["Alpha"])
+            ))
+        except Exception as e:
+            logging.warning(f"Could not register cable type '{cable['Cable Name']}': {e}. Check the definition in the cable file")
+
+    logging.info(f"{len(known_cables)} cable types registered successfully.")
+    return known_cables
+
+
+class Node:
+    def serialise(self):
+        raise NotImplementedError()
+
+    def deserialise(self):
+        raise NotImplementedError()
+
+
+@dataclass
+class Line:
+    id: int
+    name: str
+    line_from: str
+    line_to: str
+    length: float
+    type: str
+    is_active: bool = True
+    line_object: object = None
+    comment: str = ""
+
+@dataclass
+class ActiveNode(Node):
+    id: int
+    name: str
+    rating: float
+    is_active: bool = True
+    node_lv_nominal: float = 415
+    is_transformer_node: bool = True
+    node_mv_nominal: float = 11.0
+    node_object: object = None
+    comment: str = ""
+
+
+def load_nodes_from_disk(node_file: Path) -> Dict[int, ActiveNode]:
+
+    nodes = {}
+    with open(node_file, 'r') as nodes_file: 
+        reader = csv.reader(nodes_file)
+        next(reader)  # Skip header
+
+        for bus_name, transformer_rating, data_link_key, is_active, notes in reader:
+            
+            # Janky ass error checking, make this cleaner if have time
+            try:
+                node = ActiveNode(int(data_link_key), bus_name, float(transformer_rating), is_active=string_to_bool(is_active), comment=notes)
+            except ValueError as err:
+                raise ValueError(f"The bus definition for {bus_name}:{data_link_key} is malformed. Check the values for all fields. TRACE: {err}")
+
+            nodes[int(data_link_key)] = node
+
+    return nodes
+
+def load_lines_from_disk(line_file: Path) -> Dict[int, Line]:
+    lines = {}
+    with open(line_file, 'r') as lines_file: 
+        reader = csv.reader(lines_file)
+        next(reader)  # Skip header
+        for to_node, from_node, length, type, data_link_key, is_active, notes in reader:
+
+            line_name = f"FROM: {to_node}, TO: {from_node}"
+            
+            # Janky ass error checking, make this cleaner if have time
+            try:
+                line = Line(int(data_link_key), line_name, from_node, to_node, float(length), type, string_to_bool(is_active), comment=notes)
+            except ValueError:
+                raise ValueError(f"The line definition for {line_name}:{data_link_key} is malformed. Check the values for all fields")
+
+            lines[int(data_link_key)] = line
+
+    return lines
+
+def build_network(nodes: Dict[int, ActiveNode], lines: Dict[int, Line], cable_types: List[LineType]):
+    """
+    Construct a pandapower network from the specified nodes and lines. Warning: This function mutates
+    the state of the nodes and lines by linking them to the pandapower network elements
+    """
+    net = pp.create_empty_network()
+
+    # Forcefully create slack bus with ID: 0
+    nodes[0] = ActiveNode(0,"slack",0)
+
+    for cable_type in cable_types:
+        pp.create_std_type(net, {
+            "c_nf_per_km": cable_type.c_nf_per_km,
+            "r_ohm_per_km": cable_type.r_ohm_per_km,
+            "x_ohm_per_km": cable_type.x_ohm_per_km,
+            "max_i_ka": cable_type.max_i_ka,
+            "type": cable_type.type,
+            "q_mm2": cable_type.q_mm2,
+            "alpha": cable_type.alpha
+        }, name=cable_type.name, element="line")
+
+    node_name_lookup_cache = {}
+
+    for id, node in nodes.items():
+        pp_bus = pp.create_bus(net, vn_kv=node.node_mv_nominal, name=node.name, index=id)
+        node.node_object = pp_bus
+
+        if node.name in node_name_lookup_cache:
+            raise ValueError(f"Duplicate node names detected. Cannot have two nodes with the same name={node.name}")
+
+        node_name_lookup_cache[node.name] = node.id
+
+    # Now that it has been created, attach the slack bus to the external grid
+    pp.create_ext_grid(net, nodes[0].node_object)
+
+    for id, line in lines.items():
+
+        node_from = None
+        node_to = None
+
+        if line.line_from in node_name_lookup_cache:
+            node_from = nodes[node_name_lookup_cache[line.line_from]]
+        else:
+            raise ValueError(f"No existing node for name={line.line_from}")
+        
+        if line.line_to in node_name_lookup_cache:
+            node_to = nodes[node_name_lookup_cache[line.line_to]]
+        else:
+            raise ValueError(f"No existing node for name={line.line_to}")
+        
+
+        # We only tell pandapower about active lines
+        if line.is_active:
+            pp_line = pp.create_line(
+                net,
+                from_bus=node_from.node_object,
+                to_bus=node_to.node_object,
+                length_km=line.length,
+                std_type=line.type,
+                name=f"FROM: {line.name}, TO: {line.name}"
+            )
+
+            line.line_object = pp_line
+
+    return net
+
+
+def clear_network_loads(net: pd.DataFrame):
+    """
+    Helper function which clears all load definitions from a pandapower network
+    """
+    net.load.drop(net.load.index, inplace=True)
+
+
+
